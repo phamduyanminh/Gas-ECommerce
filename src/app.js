bf42bb9630e0440de33c82b517516b781c6b8276
phamduyanminh <phamduyanminh3120@gmail.com>
2024-04-27 00:19:19 -0400

    feat: add morgan, helmet, compression

--- src/app.js
+++ src/app.js
@@ -3,13 +3,30 @@
  */
 
 const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const compression = require('compression');
+
 const app = express();
 
 //Init middlewares
+//Morgan has 5 mode types: dev, combined, common, short, tiny
+//Helmet is used for protection from third parties accessing to your website looking for loop holes
+//Compression is used for improving bandwidth transmission 
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression())
 
 //Init db
 
 //Init routers
+app.get('/', (req, res, next) => {
+    const strCompress = 'Hello'
+    return res.status(200).json({
+        message: 'Welcome',
+        metadata: strCompress.repeat(100000)
+    })
+});
 
 //Handle errors
 
